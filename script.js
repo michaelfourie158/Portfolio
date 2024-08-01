cfe598e2e37d491d80358f2abb4c51375247b028
Michael Fourie <michaelfourie115588@gmail.com>
2024-08-02 00:48:09 +0200

    Added Project 1, Added experience, education, skills

--- script.js
+++ script.js
@@ -1,6 +1,4 @@
-function toggleMenu(){
-    const menu = document.querySelector(".menu-links");
-    const icon = document.querySelector(".hamburger-icon");
-    menu.classList.toggle("open");
-    icon.classList.toggle("open");
+function toggleMenu() {
+    document.querySelector(".menu-links").classList.toggle("open");
+    document.querySelector(".hamburger-icon").classList.toggle("open");
 }
